e1630a8a5a982048a1675c55adb1c31cd5fec346
Wen Jen <wjen1214@gmail.com>
2020-08-29 20:43:57 -0700

    add features to admin

---
+++ locallibrary/catalog/admin.py
@@ -0,0 +1,55 @@
+from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance, Language
+
+# Register your models here.
+
+# admin.site.register(Book)
+# admin.site.register(Author)
+admin.site.register(Genre)
+# admin.site.register(BookInstance)
+admin.site.register(Language)
+
+
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    # Use list_display to add additional fields to the view
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    # Will group horizontally if further grouped in a tuple
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
+
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    # no extra fields to add instances
+    extra = 0
+    
+
+# Does the same as the above registration method.
+# Register the Admin classes for Book using the decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    # add book instance info horizontally (tabularinine)
+    inlines = [BooksInstanceInline]
+
+
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BookInstance) 
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+    list_display = ('book', 'status', 'due_back', 'id')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
